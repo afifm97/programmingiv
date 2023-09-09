1001bd249dcd5dda146a4e764098673cac9b5f14
afifm97 <144332922+afifm97@users.noreply.github.com>
2023-09-09 11:27:53 +0800

    project

--- src/main/resources/MyDatabase.java
+++ src/main/resources/MyDatabase.java
@@ -1,6 +1,6 @@
 import java.sql.Connection;
-        import java.sql.DriverManager;
-        import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class MyDatabase {
 
@@ -10,7 +10,7 @@ public class MyDatabase {
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "root", "1234");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "root", "15disember");
         } catch (ClassNotFoundException ex) {
             ex.printStackTrace();
         } catch (SQLException ex) {
